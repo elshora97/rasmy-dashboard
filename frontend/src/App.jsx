4cbcee13540792191c27bfe6d6433e1e0191332a
Ahmed Elshora <ahmedashrafelshora@gmail.com>
2026-05-24 07:30:46 +0300

    feat: setup routing and app entry point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import './index.css';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
